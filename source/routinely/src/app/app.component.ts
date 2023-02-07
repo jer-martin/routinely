a6359368cb39fecc7aca7ba4ead9bac8a82dccd9
Jeremy Martin <48764226+jer-martin@users.noreply.github.com>
2023-02-07 13:11:28 -0500

    login button, icons

--- source/routinely/src/app/app.component.ts
+++ source/routinely/src/app/app.component.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'
+]
 })
 export class AppComponent {
   title = 'routinely';
   form = {username: '', password: '', rememberMe: false};
 
- 
+  
+
+
+  constructor(private router: Router) { }
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+  goToFilter() {
+    this.router.navigate(['/filter']);
+  }
+  goToSchedule() {
+    this.router.navigate(['/schedule']);
+  }
+  goToSidebar() {
+    this.router.navigate(['/sidebar']);
+  }
+  // goToEventModal() {
+  //   this.router.navigate(['/eventmodal']);
+  // }
   
 }
 
--- source/routinely/src/main.ts
+++ source/routinely/src/main.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+import '@cds/core/icon/register.js'
+import '@cds/core/button/register.js'
+import {ClarityIcons, cogIcon} from '@cds/core/icon';
+import {clockIcon} from '@cds/core/icon';
+ClarityIcons.addIcons(cogIcon);
+ClarityIcons.addIcons(clockIcon);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
