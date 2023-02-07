fcf180ccc7194c83a858df8fb2c24055bb0fa1f3
Jeremy Martin <48764226+jer-martin@users.noreply.github.com>
2023-02-07 13:10:54 -0500

    login button works, icons implemented

--- source/routinely/src/app/app.module.ts
+++ source/routinely/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { ClarityModule } from "@clr/angular";
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -35,4 +37,6 @@ import { FilterComponent } from './filter/filter.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
