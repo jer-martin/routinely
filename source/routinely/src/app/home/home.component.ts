4405cf09f305377e46cc9bcdc01c24fbff6a4250
Jeremy Martin <48764226+jer-martin@users.noreply.github.com>
2023-02-09 00:10:05 -0500

    calendar months + arrow + arrow anim

--- source/routinely/src/app/home/home.component.ts
+++ source/routinely/src/app/home/home.component.ts
@@ -23,4 +23,94 @@ export class HomeComponent {
   // goToEventModal() {
   //   this.router.navigate(['/eventmodal']);
   // }
+  monthNum: number | undefined;
+  month = 'January';
+
+  monthOut() {
+    return this.month;
+  }
+
+  monthNumCheck(month: string) {
+    // take in month and check against an array of every month, return index
+    // if month is not in array, return -1
+    if (this.month == null) {
+      this.month = 'January';
+    }
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+
+    for (let i = 0; i < months.length; i++) {
+      if (month === months[i]) {
+        this.monthNum = i;
+        return this.monthNum;
+      }
+    }
+    return -1;
+  }
+
+  iterateMonthUp(month: string) {
+    document.getElementById('right-arrow')!.attributes.getNamedItem('shape')!.value = 'angle-double';
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    let curIndex = this.monthNumCheck(month);
+    if (curIndex === 11) {
+      curIndex = -1;
+    }
+    this.month = months[curIndex + 1];
+    // 100 ms timer then change back to angle
+    setTimeout(() => {
+      document.getElementById('right-arrow')!.attributes.getNamedItem('shape')!.value = 'angle';
+    }, 100);
+  }
+
+  iterateMonthDown(month: string) {
+    document.getElementById('left-arrow')!.attributes.getNamedItem('shape')!.value = 'angle-double';
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    let curIndex = this.monthNumCheck(month);
+    if (curIndex === 0) {
+      curIndex = 12;
+    }
+    this.month = months[curIndex - 1];
+    // 300 ms timer then change back to angle
+    setTimeout(() => {
+      document.getElementById('left-arrow')!.attributes.getNamedItem('shape')!.value = 'angle';
+    }, 100);
+  }
 }
