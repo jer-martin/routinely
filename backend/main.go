0dcb7637e52781f68a7d1ea7b66396ee5a3272a7
bryanesquivias <101609716+bryanesquivias@users.noreply.github.com>
2023-02-28 18:29:07 -0800

    Added backend folder
    Completed user login/user creation. Needs to be integrated with database (hardcoded data for now)
    Completed unit testing for login/user creation
    Added main.go 
    Added main_test.go

---
+++ backend/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+type User struct {
+	ID             string `json:"id"`
+	UserType       string `json:"userType"`
+}
+
+
+var users = []User{
+	{ID: "bryan", UserType: "Student" },
+	{ID: "Jeremy", UserType: "Student" },
+	{ID: "Anand", UserType: "Basic"},
+	{ID: "Madhav", UserType: "Basic"},
+
+}
+
+func setUpRouter() *gin.Engine{
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	api := router.Group("/api") 
+	{
+	api.GET("/userList",getAllUsers)
+	api.POST("/createUser",createUser)
+	}
+	//router.Run("localhost:5000")
+	return router
+}
+func getAllUsers(c *gin.Context){
+	c.JSON(http.StatusOK,users)
+}
+
+// func findUser(c *gin.Context) bool{
+// 	var newUser User
+// 	for i := 0;i < len(users);i++{
+// 		if(users[i].ID == newUser.ID){
+// 			 return false
+// 		}
+// 	}
+// 	return false
+// }
+
+func createUser(c *gin.Context){
+		var newUser User
+		
+		c.BindJSON((&newUser))
+	for i := 0;i < len(users);i++{
+		if(strings.EqualFold(users[i].ID,newUser.ID)){
+			c.IndentedJSON(http.StatusBadGateway, "Username already exists , please enter new username")
+			 return
+		}
+	}
+	
+	users = append(users,newUser )
+c.IndentedJSON(http.StatusOK,"Successfuly added user")
+}
+
+func main(){
+	r := setUpRouter()
+	r.Run("localhost:5000")
+}
